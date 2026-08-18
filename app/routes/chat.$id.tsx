688806b6899eaeff1f7915243b4d46f16373a61c
7system-dot <7systemconsultoria@gmail.com>
2026-08-18 08:57:03 -0300

    Change default export from IndexRoute to BuilderRoute

--- app/routes/chat.$id.tsx
+++ app/routes/chat.$id.tsx
@@ -1,8 +1,8 @@
 import { json, type LoaderFunctionArgs } from '@remix-run/cloudflare';
-import { default as IndexRoute } from './_index';
+import { default as BuilderRoute } from './builder';
 
 export async function loader(args: LoaderFunctionArgs) {
   return json({ id: args.params.id });
 }
 
-export default IndexRoute;
+export default BuilderRoute;
